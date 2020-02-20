2f02679c2eb2bdbc73963475d6cae157ef5b5ca3
quangdx <quangdo2206@gmail.com>
2020-02-20 10:25:28 +0700

    migration

---
+++ database/migrations/2020_02_18_141023_create_borrows_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBorrowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrows', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('borrow_code');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status');
+            $table->unsignedInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrows');
+    }
+}
